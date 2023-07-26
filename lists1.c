5954c1db0bc68ee31dcab0d33e6104588c8483af
Joe Aruru <joearuru@gmail.com>
2023-07-26 18:25:06 +0300

    recommenting

--- lists1.c
+++ lists1.c
@@ -4,7 +4,7 @@
  * list_len - determines length of linked list
  * @h: pointer to first node
  *
- * Return: size of list
+ * Return: list size
  */
 size_t list_len(const list_t *h)
 {
@@ -22,7 +22,7 @@ size_t list_len(const list_t *h)
  * list_to_strings - returns an array of strings of the list->str
  * @head: pointer to first node
  *
- * Return: array of strings
+ * Return: string array
  */
 char **list_to_strings(list_t *head)
 {
@@ -84,7 +84,7 @@ size_t print_list(const list_t *h)
  * @prefix: string to match
  * @c: the next character after prefix to match
  *
- * Return: match node or null
+ * Return: match result or null
  */
 list_t *node_starts_with(list_t *node, char *prefix, char c)
 {
@@ -105,7 +105,7 @@ list_t *node_starts_with(list_t *node, char *prefix, char c)
  * @head: pointer to list head
  * @node: pointer to the node
  *
- * Return: index of node or -1
+ * Return: index of node or -1 if fail
  */
 ssize_t get_node_index(list_t *head, list_t *node)
 {
